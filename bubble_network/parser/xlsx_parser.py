b50bced9ed04c374ef6c7d6fa7c8bac4ddf67d2f
Matti Kaupenjohann <matti dot kaupenjohann at fh minus dortmund dot de>
2023-05-16 18:41:13 +0200

    finish xlsx parser

---
+++ bubble_network/parser/xlsx_parser.py
@@ -0,0 +1,32 @@
+from typing import final, Tuple
+
+import openpyxl
+from openpyxl.cell.cell import Cell
+
+from .parser import BaseParser
+
+
+class XLSXParser(BaseParser):
+    @final
+    def parse_sheet(self):
+        workbook = openpyxl.load_workbook(self.file_path)
+        num_worksheets = len(workbook.worksheets)
+
+        for worksheet in workbook.worksheets:
+            print(worksheet.title)
+            for row in range(worksheet.max_row):
+                row_cell_tuple = worksheet[row + 1]
+                if row == 0:
+                    self.table_data[worksheet.title] = {
+                        "header": self.row2tuple(row_cell_tuple)
+                    }
+                    self.table_data[worksheet.title]["rows"] = []
+                else:
+                    self.table_data[worksheet.title]["rows"].append(self.row2tuple(row_cell_tuple))
+        print()
+
+    @staticmethod
+    def row2tuple(row:Tuple[Cell]) -> Tuple[str | int]:
+        return tuple(
+            cell.value for cell in row
+        )
